fb3e66b2487e8bbb32c1318cda6cb5f1af42ceac
Yash <yashpc09@gmail.com>
2024-10-15 17:07:25 +0530

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,53 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex justify-center items-center h-screen bg-[#161616] text-white">
+      <div className="w-3/5">
+        <div className="flex flex-col mb-6">
+          <span className="font-semibold text-lg">copo.</span>
+          <span className="opacity-50 text-sm">Find flow</span>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="text-sm">
+          <b className="opacity-100">17:23</b>
+          &nbsp;
+          <span className="opacity-50">until another break...</span>
+        </div>
+        <div className="w-full flex justify-between my-2">
+          <div className="w-[63%] p-3 glass">
+            <iframe
+              className="w-full aspect-video self-stretch"
+              src="https://www.youtube.com/embed/jfKfPfyJRdk?controls=0&showinfo=0&modestbranding=1&rel=0&autohide=1"
+              allow="autoplay; encrypted-media"
+            />
+          </div>
+          <div className="w-[36%] px-5 py-3 glass text-sm">
+            <div className="opacity-80 my-2">
+              <span className="font-semibold opacity-50">rockfellar</span>{" "}
+              <span>you should study 1st law of thermodynamics</span>
+            </div>
+            <div className="opacity-80 my-2">
+              <span className="font-semibold opacity-50">rockfellar</span>{" "}
+              <span>you should study 1st law of thermodynamics</span>
+            </div>
+            <div className="opacity-80 my-2">
+              <span className="font-semibold opacity-50">rockfellar</span>{" "}
+              <span>you should study 1st law of thermodynamics</span>
+            </div>
+            <div className="opacity-80 my-2">
+              <span className="font-semibold opacity-50">rockfellar</span>{" "}
+              <span>you should study 1st law of thermodynamics</span>
+            </div>
+          </div>
+        </div>
+        <div className="opacity-50 w-2/4 text-sm my-4">
+          <span>About</span>
+          <p className="mb-6 mt-4">
+            copo is a simple, yet unique pomodoro timer: it's shared across the
+            world. this means that everyone will have breaks at the same time
+            and will follow a set (25 + 5) schedule.
+          </p>
+          <span>Built with care by @skyash</span>
+        </div>
+      </div>
     </div>
   );
 }
