9cc7e65f20a33a14b3a3cdf4598ba3e221447a4d
Yash <yashpc09@gmail.com>
2024-10-20 14:55:50 +0530

    responsive fix

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 function CenterMain() {
   return (
     <div className="flex justify-center py-4 h-full bg-[#161616] text-white">
-      <div className="w-3/5">
+      <div className="w-4/5 md:w-3/5">
         <div className="flex flex-col mb-6">
           <span className="font-semibold text-lg">copo.</span>
           <span className="opacity-50 text-sm">Find flow</span>
